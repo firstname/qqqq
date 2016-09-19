9e8dcc29e093fbaf908b6fe7bf72790441e9897d
firstname <yyychen0018@sina.com>
2016-09-19 17:28:48 +0800

    更新了首页
    首页的数据,已有的都已变成动态更新的

--- qqqq/views.py
+++ qqqq/views.py
@@ -12,8 +12,26 @@ import time
 import os
 import re
 import pandas as pd
+from qq.models import *
+from accounts.models import *
 
 # Create your views here.
 def home(request):
-    return render_to_response("index.html", RequestContext(request, { }))
+    qnrss = QnRecord.objects.all().order_by('-taken_time')
+    qnrs = qnrss[:2]
+    qnss = Questionare.objects.all().order_by('-created_time')
+    qns = qnss[:2]
 
+    users = User.objects.all()
+    stus = UserProfile.objects.filter(usergroup='is_student')
+    teas = UserProfile.objects.filter(usergroup='is_teacher')
+    return render_to_response("index.html", RequestContext(request, {
+        'qnrss':qnrss,
+        'qnss':qnss,
+        'qnrs':qnrs,
+        'qns':qns,
+
+        'users':users,
+        'stus':stus,
+        'teas':teas,
+         }))
